df2a38f6b60988bf7f99858279f3f158ba386f7c
greysmpich <greys.mpich@gmail.com>
2023-12-23 18:09:18 -0600

    test(components): se agregaron los tests para el componente Back button

--- src/app/components/back-button/back-button.component.spec.ts
+++ src/app/components/back-button/back-button.component.spec.ts
@@ -1,23 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BackButtonComponent } from './back-button.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 describe('BackButtonComponent', () => {
   let component: BackButtonComponent;
   let fixture: ComponentFixture<BackButtonComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BackButtonComponent ]
+      declarations: [ BackButtonComponent ],
+      imports: [RouterTestingModule],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(BackButtonComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router)
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return to Home when goBack is called', () => {
+    const navigateSpy = jest.spyOn(router, 'navigate');
+    component.goBack();
+    expect(navigateSpy).toHaveBeenCalledWith(['/']);
   });
 });
