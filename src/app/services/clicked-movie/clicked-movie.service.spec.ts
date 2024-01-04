1e0fdcd13ccf2bd2bc03246107f383de2acd2d73
greysmpich <greys.mpich@gmail.com>
2024-01-04 12:11:26 -0600

    test(service): se agregaron pruebas para el servicio ClickedMovie

--- src/app/services/clicked-movie/clicked-movie.service.spec.ts
+++ src/app/services/clicked-movie/clicked-movie.service.spec.ts
@@ -13,4 +13,17 @@ describe('ClickedMovieService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should set and get clicked movie ID', () => {
+    const movieId = 1206;
+    
+    service.setClickedMovieId(movieId);
+
+    let result: number | null = null;
+    service.getClickedMovieId().subscribe((id) => {
+      result = id;
+    })
+
+    expect(result).toEqual(movieId);
+  });
 });
